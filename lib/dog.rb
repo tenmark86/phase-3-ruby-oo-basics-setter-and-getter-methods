905da9f6b3b08a3b1ce338175078b3affc6da7ca
tenmark86e <tenmark86@gmail.com>
2022-10-18 10:46:36 +0300

    Done Setter and Getter Methods lab assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
-end
+    def name
+        @name
+      end
+    def name=(name)
+      @name = name
+    end
+    def breed
+        @breed
+      end
+    def breed=(breed )
+        @breed = breed
+      end
+  end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-
-end
+    def name
+        @name
+      end
+    def name=(name)
+      @name = name
+    end
+    
+    def job
+        @job
+      end
+    def job=(job)
+        @job = job
+      end
+  end
